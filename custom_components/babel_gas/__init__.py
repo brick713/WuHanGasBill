24133226b61ef8500216bdf23cb4e36266b8a877
brick713 <hibrick713@gmail.com>
2026-03-04 15:49:05 +0800

    fix bug

--- custom_components/babel_gas/__init__.py
+++ custom_components/babel_gas/__init__.py
@@ -1,3 +1,11 @@
+'''
+Author: brick713 hibrick713@gmail.com
+Date: 2025-06-11 17:00:32
+LastEditors: brick713 hibrick713@gmail.com
+LastEditTime: 2026-01-28 17:39:45
+FilePath: /WuHanGasBill/custom_components/babel_gas/__init__.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 """初始化Babel燃气集成."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
